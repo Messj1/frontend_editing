3b22d40e5ecc0316b9becba96f7364021c5f8f9c
Jan Messer <jan@mtec-studios.ch>
2021-03-23 22:12:18 +0100

    [TASK] add startup state in ToggleState [TASK][WIP] extend ToggleState by a side effect (localstorage) and two new states (locked and waiting)

--- Resources/Public/JavaScript/Component/Presentation/ToggleState.js
+++ Resources/Public/JavaScript/Component/Presentation/ToggleState.js
@@ -26,19 +26,123 @@ define([
     var log = Logger('FEditing:Component:Presentation:ToggleState');
     log.trace('--> createToggleStateComponentModule');
 
-    function createMachineDefinition (id) {
+    function createMachineDefinition (id, isEnabled=true) {
         return {
             id: id,
-            initial: 'enabled',
+            initial: 'startup',
+            context: { isEnabled },
             states: {
+                startup: {
+                    entry: [],
+                    on: { start: [{
+                            target: 'enabled',
+                            cond: ctx => ctx.isEnabled,
+                        }, {
+                            target: 'disabled',
+                            cond: ctx => !ctx.isEnabled,
+                        }]
+                    },
+                    exit: [],
+
+                },
                 enabled: {
-                    on: {toggle: 'disabled'}
+                    entry: [
+                        xstate.assign({'isEnabled': () => true})
+                    ],
+                    on: {toggle: 'disabled'},
+                    exit: [],
                 },
                 disabled: {
-                    on: {toggle: 'enabled'}
+                    entry: [
+                        xstate.assign({'isEnabled': () => false}),
+                    ],
+                    on: {toggle: 'enabled'},
+                    exit: [],
+
                 },
             },
         };
+    }
+
+    function extendDefinitionWithLocalStorageAction (definition, key) {
+        // definition.initial = 'startup';
+        // definition.context = { enabled: true };
+        definition.states.enabled.entry.push(() => {
+            localStorage.setItem(key, true);
+        });
+        definition.states.disabled.entry.push(() => {
+            localStorage.setItem(key, false);
+        });
+        definition.states.startup.entry = xstate.assign({
+            'isEnabled': () => {
+                var isEnabled = localStorage.getItem(key);
+                return isEnabled === true || isEnabled === 'true';
+            }
+        });
+    }
+
+    function extendDefinitionWithWaitingStates (definition) {
+        definition.states.waiting = {
+            on: {
+                enable: definition.states.enabled.on.toggle,
+                disable: definition.states.disabled.on.toggle,
+            }
+        };
+        definition.states.locked = {
+            on: {
+                unlock: [{
+                    target: 'enabled',
+                    cond: ctx => ctx.isEnabled,
+                }, {
+                    target: 'disabled',
+                    cond: ctx => !ctx.isEnabled,
+                }],
+            },
+            exit: xstate.assign({
+                'isLocked': false
+            }),
+        };
+        definition.context.isLocked = false;
+        definition.states.disabled.exit.push((ctx, event) => {
+            console.log('states.disabled.exit', ctx, event);
+        });
+        definition.states.disabled.exit.push(xstate.assign({
+            'isLocked': true
+        }));
+        definition.states.disabled.exit.push();
+        // definition.states.enabled.on.toggle = 'waiting';
+        definition.states.enabled.on.lock= 'locked';
+        definition.states.disabled.on.toggle = 'waiting';
+    }
+
+    function registerWaitingStateHandler (service) {
+        var currentState;
+
+        return service.subscribe((state) => {
+            log.debug('WaitingState handler', state);
+
+            if(state.value === currentState) {
+                return;
+            }
+
+            if (state.value === 'enabled' && state.context.isLocked) {
+                log.info('change state to locked');
+
+                window.setTimeout(() => service.send('lock'), 0);
+            } else if(state.value === 'locked') {
+                log.info('handle locked state', state.value);
+
+                window.setTimeout(() => service.send('unlock'), 1000);
+            } else if(state.value === 'waiting') {
+                log.info('handle waiting state', state.value);
+
+                window.setTimeout(() => service.send(
+                    state.context.isEnabled ? 'enable' : 'disable'
+                ), 1000);
+            }
+
+            currentState = state.value;
+        });
     }
 
     return function createToggleStateComponent (config) {
@@ -48,14 +152,30 @@ define([
         var id = config.name ? config.name : 'toggle';
 
         var machineDefinition = createMachineDefinition(id);
+
+        extendDefinitionWithLocalStorageAction(machineDefinition, 'rightPanelEnabled');
+        extendDefinitionWithWaitingStates(machineDefinition);
+
         var machine = xstate.createMachine(machineDefinition);
         var service = xstate.interpret(machine).start();
 
         service.subscribe((state) => {
-            log.info('state toggled', id, state.value);
+            if(state.value === currentState) {
+                log.info('pre toggle state', id, state.value);
+                return;
+            }
 
+            log.info('toggle state', id, state.value);
+
+            if(state.value === 'startup') {
+                window.setTimeout(() => service.send('start'), 0);
+            }
+
+            log.debug('currentState', currentState);
             currentState = state.value
         });
+
+        registerWaitingStateHandler(service);
 
         return {
             subscribe: service.subscribe,
